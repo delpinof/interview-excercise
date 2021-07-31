bb895ec0c81840e2f26abf001665d0d208e1a030
Fernando del Pino <fherdelpino@hotmail.com>
2021-07-31 09:17:48 -0500

    initial commit

---
+++ src/main/java/com/improving/interviewexcercise/InterviewExcerciseApplication.java
@@ -0,0 +1,13 @@
+package com.improving.interviewexcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewExcerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewExcerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/improving/interviewexcercise/InterviewExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.improving.interviewexcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
