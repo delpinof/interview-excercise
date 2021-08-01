21386af02fcef8a5a23050be4f79bd68822f6eb8
Fernando del Pino <fherdelpino@hotmail.com>
2021-07-31 23:26:31 -0500

    remove parent src

--- src/main/java/com/improving/interviewexcercise/dao/ReservationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.improving.interviewexcercise.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ReservationRepository extends CrudRepository<ReservationEntity, Integer> {
-}
--- src/main/java/com/improving/interviewexcercise/service/ReservationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.improving.interviewexcercise.service;
-
-public class ReservationNotFoundException extends Exception {
-    public ReservationNotFoundException(String message) {
-        super(message);
-    }
-}
